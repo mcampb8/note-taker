9154db2be1b2baab9cb81d7f2a1b806bbb2bfd3a
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-04-27 21:28:13 -0700

    working get and post reqs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-const termData = require('./Develop/db/db.json');
+const noteData = require('./Develop/db/db.json');
+const uuid = require ('./Develop/public/assets/js/uuid');
+const note_id =0;
 const PORT = 3001;
 
 const app = express();
@@ -10,11 +12,43 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+app.get('/api/notes', (req, res) => res.json(noteData));
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
 
-app.get('/notes',  (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+});
+
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './develop/public/notes.html'));
-  });
+});
 
-  app.listen(PORT, () => {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+});
+app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
-  });
+});
