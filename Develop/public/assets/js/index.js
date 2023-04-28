b3b3bf0dfd8d3b6dc984127e629f3a53af21fd99
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-04-27 22:54:07 -0700

    updates

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -180,4 +180,4 @@ if (window.location.pathname === '/notes') {
   noteText.addEventListener('keyup', handleRenderSaveBtn);
 }
 
-getAndRenderNotes();
+module.exports = getAndRenderNotes();
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const noteData = require('./Develop/db/db.json');
 const uuid = require ('./Develop/public/assets/js/uuid');
+const indexjs = require('./Develop/public/assets/js/index');
 const note_id =0;
 const PORT = process.env.PORT || 3001;
 
