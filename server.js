ccfde3d3963040a04441e7ec9525a99909939125
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-04-29 10:04:00 -0700

    fixed file paths

--- server.js
+++ server.js
@@ -50,11 +50,11 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
 app.listen(PORT, () => {
