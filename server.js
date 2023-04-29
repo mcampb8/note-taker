e1d4bdc6166ff8c45fc79b21966e04b489b96823
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-04-29 09:50:55 -0700

    fixed pull request

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const noteData = require('./Develop/db/db.json');
 const uuid = require ('./Develop/public/assets/js/uuid');
-const indexjs = require('./Develop/public/assets/js/index');
-const note_id =0;
+const fs = require ('fs');
+const id =0;
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -12,35 +12,41 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
-app.get('/api/notes', (req, res) => res.json(noteData));
+app.use(express.static('Develop/public'));
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./Develop/db/db.json',"UTF-8",(error,data) =>{
+        res.json(JSON.parse(data))});
+})
 app.post('/api/notes', (req, res) => {
     // Log that a POST request was received
     console.info(`${req.method} request received to add a note`);
 
     // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
-
-    // If all the required properties are present
+    fs.readFile('./Develop/db/db.json',"UTF-8", (error,data) =>  {
+        console.log(data);
+        // If all the required properties are present
     if (title && text) {
         // Variable for the object we will save
         
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            id: uuid(),
         };
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in posting note');
-    }
+        data = JSON.parse(data);
+        data.push(newNote);
+        fs.writeFile('./Develop/db/db.json', JSON.stringify(data), (error) =>{
+            if(error){
+            res.status(500).json('Error in posting note');
+            }
+            else{
+            res.status(201).json(newNote);
+            }
+        })
+        
+    } 
+    })
 });
 
 app.get('/notes', (req, res) => {
@@ -50,6 +56,7 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './develop/public/index.html'));
 });
+
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 });
